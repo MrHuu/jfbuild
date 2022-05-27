e52adca6be47d8e807ae96858c1e41d8682d4f7b
BSzili <col.lawrence@gmail.com>
2022-05-27 07:14:11 +0200

    ldiv id now working with GCC6, added pow replacement from clib2

--- src/amilayer.c
+++ src/amilayer.c
@@ -1902,7 +1902,6 @@ int handleevents(void)
 char *getenv(const char *value) { return NULL; }
 // NOTES-3.4.0
 int (putchar)(int c) { return fputc(c,stdout); }
-#endif
 // disable the default CTRL-C handler
 //void __chkabort(void) {}
 // replacement for the bugged ldiv
@@ -1918,8 +1917,84 @@ ldiv_t ldiv(long num, long denom)
 	}
 	return r;
 }
+#endif
 // minimum stack
 extern void __stkinit(void);
 void * __x = __stkinit;
 unsigned long __stack = (100*1024);
+// pow replacement
+#ifdef __HAVE_68881__
+/*
+ * Portable ISO 'C' (1994) runtime library for the Amiga computer
+ * Copyright (c) 2002-2015 by Olaf Barthel <obarthel (at) gmx.net>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   - Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *   - Neither the name of Olaf Barthel nor the names of contributors
+ *     may be used to endorse or promote products derived from this
+ *     software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+inline static double
+__exp(double x)
+{
+	double result;
+
+	__asm ("fetox%.x %1,%0"
+	       : "=f" (result)
+	       : "f" (x));
+
+	return(result);
+}
+
+inline static double
+__log(double x)
+{
+	double result;
+
+	__asm ("flogn%.x %1,%0"
+	       : "=f" (result)
+	       : "f" (x));
+
+	return(result);
+}
+
+double pow(double x,double y)
+{
+	double result;
+
+	if(x > 0)
+	{
+		result = __exp(y * __log(x));
+	}
+	else
+	{
+		int i = (int)y;
+
+		if((i & 1) == 0) /* even */
+			result = __exp(y) * __log(-x);
+		else
+			result = -(__exp(y) * __log(-x));
+	}
+
+	return(result);
+}
+#endif
 #endif
